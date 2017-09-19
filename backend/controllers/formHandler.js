bdead256240fdce6535ea647d6dee96fc4dc4467
YuChao Dong <yuchaodong@gmail.com>
2017-09-19 12:03:18 -0400

    setup routes

---
+++ backend/controllers/formHandler.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const Sequelize = require('sequelize');
+// express, take req, res, manipulate db
+
+
+ // input Name, Type, Date, Location
+
+ // get body object
+ // sequelize model
+
--- backend/router/routes.js
+++ backend/router/routes.js
@@ -1,1 +1,14 @@
+const express = require('express');
 const router = require('express').Router();
+
+const signupHandler = require('../controllers/signupHandler');
+const loginHandler = require('../controllers/loginHandler');
+const formHandler = require('../controllers/formHandler');
+
+router.post('/signup', signupHandler);
+
+// router.get('/login', loginHandler);
+
+// router.get('/logout'); //need to destroy session
+
+router.post('/form', formHandler);
