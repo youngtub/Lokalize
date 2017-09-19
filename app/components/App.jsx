6e5b4de884b7c5bb884435f5f73bc97dc7ef9ce9
Alexander Lukens <alexander.lukens1@gmail.com>
2017-09-19 11:49:04 -0400

    Add guest signup form

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './Header.jsx';
+import Search from './Search.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
+
   }
   render() {
-    return <Header />
+    return (
+      <div>
+        <Header />
+        <Search />
+      </div>
+    )
   }
 };
 
--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -3,10 +3,41 @@ import React from 'react';
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      address: '',
+      date: '',
+      type: ''
+    }
   }
+
+  handleSubmit(event) {
+    console.log(this.state)
+    event.preventDefault()
+  }
+
+  handleChange(event){
+    let property = event.target.className;
+    let state = {};
+    state[property] = event.target.value;
+    this.setState(state)
+  }
+
   render() {
-    return <div>Lokalize!!</div>
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+      Enter Address: <input type = "text" className='address' onChange={this.handleChange.bind(this)}/><br/>
+      Select Dinner Type: <input list="food" className='type' onChange={this.handleChange.bind(this)}/>
+      <datalist id="food">
+        <option value="Seafood"/>
+        <option value="Steak"/>
+        <option value="Chicken"/>
+        <option value="Vegetarian"/>
+        <option value="No Preference"/>
+      </datalist><br/>
+      Select Date: <input type = "date" className='date' onChange={this.handleChange.bind(this)}/><br/>
+      <input type="submit"/>
+      </form>
+    )
   }
 };
 
