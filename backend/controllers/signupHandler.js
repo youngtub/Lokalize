1b6469564164e007c192ad65c4bab0fede8c4b48
ANR <anr@Owners-MacBook-Pro.local>
2017-09-19 20:42:30 -0400

    Zomato location cityid api call working

--- backend/controllers/signupHandler.js
+++ backend/controllers/signupHandler.js
@@ -2,20 +2,25 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const users = require('../models/userSchema');
 const parser = require('body-parser');
+const axios = require('axios');
 
 exports.handleSignup = (req, res) => {
   var user = {};
   user['username'] = req.body.username;
   user['password'] = req.body.password;
+  user['cityName'] = req.body.cityName;
   var usernameAlreadyInUse = users.findOne({where: {username: user.username}})
-  .then((usernameAlreadyInUse) => {
+  .then( (usernameAlreadyInUse) => {
     if (usernameAlreadyInUse) {
-      res.send('username already in use, please pick another username');
-      res.redirect('/signup');
-    } else {
+      return res.send('username already in use, please pick another username');
+      // res.redirect('/signup');
+    }
+    locationAPICall(user.cityName)
+    .then( (cityId) => {
       var newUser = users.create({
         username: user.username,
-        password: user.password
+        password: user.password,
+        cityId: cityId
       })
       .then((newUser) => {
         if (newUser) {
@@ -24,6 +29,24 @@ exports.handleSignup = (req, res) => {
           console.error('Error:', err);
         }
       })
-    }
+    })
   })
 };
+
+var locationAPICall = (locationString) => {
+  var location = locationString.replace(/ /g, '%20');
+  var reqUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + location;
+  var config = {
+    "headers" : {
+  	   "Content-Type": "application/json",
+  	    "user-key": "0531c898c316947b94f8b79453e43caf"
+      }
+    };
+  return axios.get(reqUrl, config)
+  .then( (results) => {
+    console.log('RESULTS', results)
+  	return cityId = results.data.location_suggestions[0].entity_id;
+  })
+  .catch( (err) => console.error(err));
+
+}
--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -9,6 +9,7 @@ const User = sequelize.define('users', {
   },
   username: Sequelize.STRING,
   password: Sequelize.STRING,
+  cityId: Sequelize.INTEGER,
 }, {
   timestamps: false
 })
