ffa347dd0bc86508c39d5834edf7c50df5ce9f02
ANR <anr@Owners-MacBook-Pro.local>
2017-09-20 17:06:57 -0400

    working

--- app/components/Create.jsx
+++ app/components/Create.jsx
@@ -4,6 +4,7 @@ import ListEntryCreate from './subComponents/ListEntryCreate.jsx';
 import { Container, Jumbotron } from 'react-bootstrap';
 import axios from 'axios';
 
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -33,18 +34,20 @@ class Home extends React.Component {
   submitEvent(name, type, date) {
     console.log('ABOUT TO SEND POST REQUEST');
     console.log('Parameters', name, type, date, this.state.venue, this.state.address);
-    axios.post('/api/form', {
-      name: name,
-      dinnerType: type,
-      date: date,
-      location: this.state.venue,
-      address: [40.7504864, 73.97640100000001],
-      capacity: 10
-    })
-    .then( (response) => {
-      // console.log('Event sent to back end');
-      console.log(response);
-    })
+
+      axios.post('/api/form', {
+        name: name,
+        dinnerType: type,
+        date: date,
+        location: this.state.venue,
+        address: this.state.address,
+        capacity: 10,
+        userid: this.props.userid
+      })
+      .then( (response) => {
+        // console.log('Event sent to back end');
+        console.log(response);
+      })
   }
 
   render() {
