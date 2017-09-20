154ed6709dae0346c9b8ba626af8ddd79646c8aa
Alexander Lukens <alexander.lukens1@gmail.com>
2017-09-19 21:42:02 -0400

    fix routes error

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -11,8 +11,7 @@ router.post('/signup', signupHandler.handleSignup);
 router.post('/login', loginHandler.handleLogin);
 
 // router.get('/logout'); //need to destroy session
-
-//router.post('/form', formHandler);
+router.post('/form', formHandler.handleForm);
 
 router.post('/search', searchHandler.searchEvents);
 
