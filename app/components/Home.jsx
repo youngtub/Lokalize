152f67dc7d53b4e52a2f160ff7b41b2f05c6151e
Katie <krblacketor@gmail.com>
2017-09-20 15:12:41 -0400

    Start server side of event list.

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Weather from './subComponents/Weather.jsx';
 import Search from './subComponents/Search.jsx';
 import MapWithADirectionsRenderer from './subComponents/Map.jsx';
@@ -9,8 +10,19 @@ import { Container, Jumbotron } from 'react-bootstrap';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      events: []
+    }
   }
-
+  //call to server to database to retrieve the events
+  getEvents() {
+    let username = this.state.username;
+    return axios.get('/home')
+      .then((response) => {
+        this.setState({events})
+      })
+  }
   render() {
     return (
       <div>
