b8c600345dd063fb6da4dc5fd7e55f50e3f864a3
ANR <anr@Owners-MacBook-Pro.local>
2017-09-20 17:13:44 -0400

    good changes

--- backend/controllers/formHandler.js
+++ backend/controllers/formHandler.js
@@ -11,27 +11,37 @@ exports.handleForm = (req, res) => {
   data["name"] = req.body.name;
   data["dinnerType"] = req.body.dinnerType;
   data["date"] = req.body.date;
-  data["capacity"] = req.body.capacity;
   data["location"] = req.body.location;
   data["address"] = req.body.address;
-  var exists = events.findAll({data})
-  .then( (exists) => {
-    if (exists) {
-      return res.send('Event already exists!');
-   }});
-  var newEvent = events.create({
-    name: data.name,
-    dinner_type: data.dinnerType,
-    date: data.date,
-    capacity: data.capacity,
-    location: data.location,
-    address: data.address
-  })
-  .then((newEvent) => {
-    if(newEvent) {
-      res.send('Event created!');
-    } else {
-      console.error('Error: ', err);
+  data["capacity"] = req.body.capacity;
+  events.findAll({where:
+    {
+      name: data.name,
+      location: data.location,
+      date: data.date
     }
   })
-  };
+  .then((exists) => {
+    if (exists.length !== 0) {
+      res.send('Event already exists!');
+      return
+    }
+    events.create({
+      name: data.name,
+      dinner_type: data.dinnerType,
+      date: data.date,
+      location: data.location,
+      address: data.address,
+      capacity: data.capacity
+    })
+    .then((data) => {
+      res.send('Event Created')
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  });
+};
