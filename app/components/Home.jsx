0ebe43086ad040cc46df3e20b483a8f9808a9b58
alexanderlukens <alexander.lukens1@gmail.com>
2017-09-20 13:37:41 -0400

    Update Home.jsx

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -3,7 +3,7 @@ import Weather from './subComponents/Weather.jsx';
 import Search from './subComponents/Search.jsx';
 import MapWithADirectionsRenderer from './subComponents/Map.jsx';
 // import Calendar from './subComponents/Calendar.jsx';
-//import ListEntry from './subComponents/ListEntry.jsx';
+import ListEntry from './subComponents/ListEntry.jsx';
 import { Container, Jumbotron } from 'react-bootstrap';
 
 class Home extends React.Component {
