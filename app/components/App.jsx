25b9151d86c4ecbba00b186a0b003d46a34f08f4
alexanderlukens <alexander.lukens1@gmail.com>
2017-09-21 11:05:42 -0400

    Update App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -50,7 +50,6 @@ class App extends React.Component {
       password: password
     })
     .then((res) => {
-      console.log('RES IN APP.jsx', res)
       if (res.data) {
         this.setState({
           isLoggedIn: true,
