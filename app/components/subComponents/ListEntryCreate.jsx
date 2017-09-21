7a929a5bbc43a302193839cc1c89b8b805d34c7e
ANRanganathan <anranga95@gmail.com>
2017-09-20 20:35:45 -0400

    Fixed ListEntryCreate.js

--- app/components/subComponents/ListEntryCreate.jsx
+++ app/components/subComponents/ListEntryCreate.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> formdata
 import $ from 'jquery';
 //list
 
@@ -21,44 +16,11 @@ const ListEntryCreate = (props) => {
     <div id='options'>
       {props.entries.map( (entry, ind) => (
         <div style={itemStyle} onClick={handleSelect} key={ind} id={entry.restaurant.name} className={entry.restaurant.location.address} >
-<<<<<<< HEAD
-=======
-=======
-import $ from 'jquery';
->>>>>>> merge conflicts fixed
-//list
-
-const ListEntryCreate = (props) => {
-
-  const handleSelect = (e) => {
-    var venue = $(e.target).attr('id');
-    var address = $(e.target).attr('class');
-    console.log('HOST SELECTED', venue + ' ' + address)
-    $(e.target).css('background-color', 'grey' );
-    props.selectCallback(venue, address);
-  }
-
-  return (
-<<<<<<< HEAD
-    <div>
-      {props.entries.map( (entry) => (
-        <span>
->>>>>>> working on selecting venue
-=======
-    <div id='options'>
-      {props.entries.map( (entry, ind) => (
-        <div style={itemStyle} onClick={handleSelect} key={ind} id={entry.restaurant.name} className={entry.restaurant.location.address} >
->>>>>>> merge conflicts fixed
-=======
->>>>>>> formdata
             Name: {entry.restaurant.name}<br></br>
             Cuisine: {entry.restaurant.cuisines}<br></br>
             Locality: {entry.restaurant.location.locality}<br></br>
             Rating: {entry.restaurant.user_rating.aggregate_rating}<br></br>
             <img src={entry.restaurant.thumb} width={100} height={100}></img><br></br>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       </div>
         ))}
   </div> )
@@ -71,33 +33,5 @@ const itemStyle = {
   padding: '10px',
   textAlign: 'center'
 }
-
-=======
-            <hr></hr>
-      </span>
-        ))}
-  </div> )
-};
->>>>>>> working on selecting venue
-=======
-=======
->>>>>>> formdata
-      </div>
-        ))}
-  </div> )
-}
-
-const itemStyle = {
-  border: 'solid black',
-  borderWidth: '1px',
-  borderRadius: '70px',
-  padding: '10px',
-  textAlign: 'center'
-}
-
-<<<<<<< HEAD
->>>>>>> merge conflicts fixed
-=======
->>>>>>> formdata
 
 export default ListEntryCreate;
