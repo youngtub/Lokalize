205e71e871c1b69dc15fc967f19bd378def3b01f
ANR <anr@Owners-MacBook-Pro.local>
2017-09-21 15:59:55 -0400

    event form complete

---
+++ app/lib/idReferencesForApi.js
@@ -0,0 +1,34 @@
+exports.cuisines = {
+  "American": "1",
+  "Asian": "3",
+  "BBQ": "193",
+  "Breakfast": "182",
+  "Burger": "168",
+  "Cafe": "30",
+  "Diner": "541",
+  "Fusion": "274",
+  "Italian": "55",
+  "Mexican": "73",
+  "Pizza": "82",
+  "Seafood": "83",
+  "Steak": "141",
+  "Vegetarian": "308"
+}
+
+exports.localities = {
+  "Chelsea": "170247",
+  "East Village": "114202",
+  "Financial District": "114193",
+  "Greenwich Village": "114136",
+  "Gramercy-Flatiron": "170300",
+  "Midtown": "170244",
+  "Soho": "114199",
+  "Tribeca": "114166",
+  "Union Square": "161718",
+  "Upper East Side": "114160",
+  "Upper West Side": "114151",
+  "Lower East Side": "114196",
+  "Hell's Kitchen": "114130"
+}
+
+//entity_type = 'subzone'
