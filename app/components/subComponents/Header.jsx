229a96c58ade4234a270712240ca9ed631772263
Katie <krblacketor@gmail.com>
2017-09-22 15:53:40 -0400

    little big things

--- app/components/subComponents/Header.jsx
+++ app/components/subComponents/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Navbar, Nav, NavItem, PageHeader, Panel, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import glamorous from 'glamorous';
+import bUtils from 'react-bootstrap/lib/utils/bootstrapUtils';
+// bUtils.addStyle(Navbar, ['home']);
 
 class Header extends React.Component {
   constructor(props) {
@@ -23,27 +26,32 @@ class Header extends React.Component {
   render() {
     return (
       <Panel className="nav-bar">
+        <span className="nav-bar" >
         <Navbar inverse collapseOnSelect fixedTop fluid>
-          <Navbar.Header>
+          <Navbar.Header color="#4A0D04">
             <Navbar.Brand>
               <Link to="/home">Lokalize</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            {this.props.isLoggedIn ?
+          {this.props.isLoggedIn ?
           <Nav>
             <NavItem eventKey={1} href="#"><Link to="/join">Join Event</Link></NavItem>
             <NavItem eventKey={2} href="#"><Link to="/host">Host Event</Link></NavItem>
           </Nav> : ''}
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#" onClick={this.props.onLogout}>Log Out</NavItem>
-        </Nav>
-          </Navbar.Collapse>
+          <Nav pullRight>
+          {this.props.isLoggedIn ?
+            <NavItem eventKey={1} href="#" onClick={this.props.onLogout}>Log Out</NavItem>
+            : ''}
+          </Nav>
+            </Navbar.Collapse>
         </Navbar>
         <div className="Header">
-          <PageHeader> LOKALIZE <small>Random Meetup Generator</small></PageHeader>
+          <PageHeader> LOKALIZE <small>New York City</small></PageHeader>
         </div>
+        </span>
+
       </Panel>
     )
   }
