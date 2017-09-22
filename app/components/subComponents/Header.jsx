c3485c452daae3b29210ae471bb13b2b0fb343a6
YuChao Dong <yuchaodong@gmail.com>
2017-09-22 10:58:32 -0400

    username and password restrictions working

--- app/components/subComponents/Header.jsx
+++ app/components/subComponents/Header.jsx
@@ -29,13 +29,13 @@ class Header extends React.Component {
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#"><Link to="/join">Join Event</Link></NavItem>
-              <NavItem eventKey={2} href="#"><Link to="/host">Host Event</Link></NavItem>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#" onClick={this.props.onLogout}>Log Out</NavItem>
-            </Nav>
+        <Nav>
+          <NavItem eventKey={1} href="#"><Link to="/join">Join Event</Link></NavItem>
+          <NavItem eventKey={2} href="#"><Link to="/host">Host Event</Link></NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#" onClick={this.props.onLogout}>Log Out</NavItem>
+        </Nav>
           </Navbar.Collapse>
         </Navbar>
         <div className="Header">
